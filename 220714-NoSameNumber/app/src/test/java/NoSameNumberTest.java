2c959f5e61851cb33902c3f51adada0f3a3cf313
myunghoonkim <xcv8826@gmail.com>
2022-07-14 11:12:21 +0900

    20220714-NoSameNumber
    못품
    
    다른 아이디어 참고해서 일단 이애하고 품
    
    복습필요
    
    힌트 배열의 요소가 앞 배열과 같지 않다면 더한다 -> 즉 중복 아닐시 더해줌

---
+++ 220714-NoSameNumber/app/src/test/java/NoSameNumberTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NoSameNumberTest {
+  @Test
+  void sequence() {
+    NoSameNumber noSameNumber = new NoSameNumber();
+
+
+
+  }
+
+}

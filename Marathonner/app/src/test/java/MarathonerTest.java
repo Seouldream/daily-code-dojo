725fcc13fd2afb7dd0237f21da1f03bb93d79e06
myunghoonkim <xcv8826@gmail.com>
2022-07-30 15:40:37 +0900

    20220730 coding-dojo-update

--- Marathonner/app/src/test/java/MarathonerTest.java
+++ Marathonner/app/src/test/java/MarathonerTest.java
@@ -1,4 +1,14 @@
+import org.junit.jupiter.api.*;
+
+import java.util.*;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class MarathonerTest {
-  
+  @Test
+  void booleanTest() {
+    Marathoner marathoner = new Marathoner();
+
+  assertTrue(marathoner.sameName(new String[] {"이너","이누"}));
+  }
 }
